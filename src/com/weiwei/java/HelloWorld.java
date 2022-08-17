1e4e2910a5eec6db5c5b22b0b1655ecdb7a3015b
fankw <111@11.com>
2022-08-17 20:01:13 +0800

    Initial commit

---
+++ src/com/weiwei/java/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.weiwei.java;
+
+
+}
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+    }
+
+
+}
+
---
+++ src/com/weiwei/java/hello.java
@@ -0,0 +1,8 @@
+package com.weiwei.java;
+
+/**
+ * @author fanxiaowei
+ * @date 2022/8/17- 17:11
+ */
+public class hello {
+}
